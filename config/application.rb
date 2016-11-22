3a8ac8283f4cba20c880b872b0f70a24e00bb9e5
saitou takashi <bookman2710@gmail.com>
2016-11-22 05:39:19 +0000

    トップページ完成

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,7 @@ module Tsubuyaki
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.sass.preferred_synatax = :sass
+
+    config.i18n.default_locale = :ja
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resource :registrations, only: [:new, :create]
 
   resources :users
+
+  root to: 'registrations#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
