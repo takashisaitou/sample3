2575e74c25921a16325534d149ae797ee5c1e035
saitou takashi <bookman2710@gmail.com>
2016-11-22 09:42:30 +0000

    シラバス４番まで終わり

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :creat, :destroy]
 
-  resources :users
+  resources :users, only: [:index, :show]
 
   root to: 'registrations#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
