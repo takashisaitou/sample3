2edccb803866bf04a3d0d64b3c902838dcd7851b
saitou takashi <bookman2710@gmail.com>
2016-11-23 01:35:27 +0000

    Settings#editのスタイル変更まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   resource :registrations, only: [:new, :create]
-  resource :sessions, only: [:new, :creat, :destroy]
+  resource :sessions, only: [:new, :create, :destroy]
+  resource :settings, only: [:edit, :update]
 
   resources :users, only: [:index, :show]
 
